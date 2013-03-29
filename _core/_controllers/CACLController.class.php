d53706bf556f430c68e0b45edb72f26b0b12656b
Илья <kazakow93@gmail.com>
2013-03-29 18:22:59 +0500

    Пробный коммент

--- _core/_controllers/CACLController.class.php
+++ _core/_controllers/CACLController.class.php
@@ -6,6 +6,7 @@
  * Time: 17:40
  * To change this template use File | Settings | File Templates.
  */
+/**/
 class CACLController extends CBaseController {
     private $allowedAnonymous = array(
         "restorePassword",
